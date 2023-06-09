bde658a2750e25334fe590e05d3334b82d0844a0
OJEDA ROSAS JONATHAN ARATH <zs20006736@estudiantes.uv.mx>
2023-06-08 22:55:26 -0500

    babel

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
